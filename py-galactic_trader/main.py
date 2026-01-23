ff16026729132714d29f4395ed1788fb959f29f4
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2026-01-23 12:11:37 +0300

    fix: major improvements

--- py-galactic_trader/main.py
+++ py-galactic_trader/main.py
@@ -1,3 +1,4 @@
+import copy
 from rule import starting_rules, reasoned_rules
 from world import *
 
@@ -5,32 +6,50 @@ class State:
     def __init__(self, context, money):
         self.context = context
         self.money = money
+    
+    def __str__(self):
+        return f"Context: {self.context}, Money: {self.money}"
 
-rules = starting_rules
-state = State(context={"planet": "A"}, money=100)
+if __name__ == "__main__":
+    rules = starting_rules
+    state = State(context={"planet": "A"}, money=100)
 
-for i in range(50):
-    # Add a newly reasoned rule
-    if reasoned_rules:
-        rules.append(reasoned_rules.pop())
+    for i in range(2000):
+        # Add a newly reasoned rule
+        if reasoned_rules:
+            rules.append(reasoned_rules.pop())
 
-    applicable_rules = []
-    for rule in rules:
-        if not is_valid_rule(rule, state):
+        applicable_rules = []
+        for rule in rules:
+            if not is_valid_rule(rule, state):
+                continue
+            
+            sampled_value = rule.get_sample_value()
+            applicable_rules.append((rule, sampled_value))
+
+        print(f"Current State: {state}")
+        for rule in rules:
+            print(rule.name, end=", ")
+        print()
+        if not applicable_rules:
+            print("INFO: Couldn't find any applicable rules")
             continue
         
-        sampled_value = rule.get_sample_value()
-        applicable_rules.append((rule, sampled_value))
+        sorted_rules = sorted(applicable_rules, key= lambda x: x[1], reverse=True)
+        print(sorted_rules)
+        chosen_rule = sorted_rules[0][0]
+        print(f"Chosen Rule: {chosen_rule.name}")
 
-    if not applicable_rules:
-        print("INFO: Couldn't find any applicable rules")
-        continue
-    
-    chosen_rule = sorted(applicable_rules, key= lambda x: x[1], reverse=True)[0][0]
-    new_state = execute_rule(chosen_rule, state)
-    reward = evaluate_state(new_state, state)
-    chosen_rule.update(reward)
-    state = new_state
-    print(f"Step {i}: {chosen_rule.name} -> {reward}")
+        # new_state = execute_rule(chosen_rule, state)
+        new_state = execute_rule(chosen_rule, copy.deepcopy(state))
 
-    print(state.money)
+        print(f"New State: {new_state}")
+        reward = evaluate_state(new_state, state)
+        chosen_rule.update(reward)
+        state = new_state
+        print(f"Step {i}: {chosen_rule.name} -> {reward}")
+
+        print(state.money)
+        if state.money <= 0:
+            print('Ran out of money on iteration:', i)
+            break
